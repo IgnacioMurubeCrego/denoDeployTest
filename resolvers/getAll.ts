d851a2ad1a0e5f860a2fbd2e5683c5dbe0dae2ab
IgnacioMurubeCrego <imurubec@alumnos.nebrija.es>
2023-10-28 12:38:42 +0200

    Updated import error comments

--- resolvers/getAll.ts
+++ resolvers/getAll.ts
@@ -1,8 +1,8 @@
-// @ts-ignore
+// @ts-ignore ignored import error
 import {Request, Response} from "npm:express@4.18.2";
 import { CharacterModel, CharacterModelType } from "../db/character.ts";
 
-export const getAll = async (req:Request, res : Response) => {
+export const getAll = async (_req:Request, res : Response) => {
     try{
     const characters : CharacterModelType[] = await CharacterModel.find();
     res.send(characters);
